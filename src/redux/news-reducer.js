2143eadc0b52e00aa088a64f992d4ef6603e6afb
Xromer <tarasoviv@bigmir.net>
2020-05-19 21:41:30 +0300

    correct newsPage

---
+++ src/redux/news-reducer.js
@@ -0,0 +1,12 @@
+let initialState = {
+    news: [
+        { id: 1, news: 'Коронавирус в Украине: количество зафиксированных случаев на 3 мая' }
+    ]
+};
+
+const newsReducer = (state = initialState, action) => {
+    return state;
+
+}
+
+export default newsReducer;
