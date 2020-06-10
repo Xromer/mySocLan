1fb6a3e216018824ca1dbfa543ca50b46b0cfcdb
Igor.Tarasov <tarasoviv@bigmir.net>
2020-06-10 17:11:09 +0300

    change ava

--- src/components/profile/profileInfo/profileInfo.js
+++ src/components/profile/profileInfo/profileInfo.js
@@ -11,10 +11,20 @@ const ProfileInfo = (props) => {
     return (
         <div className={classes.profile}>
             <div className={classes.myphoto}>
-                <img className={classes.logo2} src={props.profile.photos.large} />
+
+                <img className={classes.logo2} src={props.profile.photos.large != null ? props.profile.photos.large : 'https://pm1.narvii.com/6569/7b98dc403996d3146789d0e6dbbc92dd17d036f2_00.jpg'} />
+
             </div>
             <div className={classes.aboute_me}>
-                Opisanie
+                <div>
+                    Full Name: {props.profile.fullName}
+                </div>
+                <div>
+                    {props.profile.lookingForAJobDescription}
+                </div>
+                <div>
+                    {props.profile.aboutMe}
+                </div>
             </div>
         </div>
     )
